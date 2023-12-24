916081631a47c35c2eca5c91c090bf28439bf176
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2023-12-24 17:42:05 -0300

    Adição da Data Final feat(data_final): Implementado o salvamento da data limite de resgate do jogo gratuito. docs(readme): Adição do README provisório do projeto

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1>Web Scraping - Epic Games Store</h1>
+<h2>Introduction</h2>
+<p>Web scraping is a technique to automatically access and extract large amounts of information from a website, which can save a huge amount of time and effort. In this project, I will scrape the data from the Epic Games Store website and store it in a structured format for later analysis.</p>
+
+<h2>Project Structure</h2>
+<ul>
+<li><b>epic_games_store_scraping.ipynb</b> - Jupyter notebook with the code for scraping the data</li>
+<li><b>epic_games_store.csv</b> - CSV file with the scraped data</li>
+</ul>
+
+<h2>Code</h2>
+<p>The code is written in Python 3.7.3. If you don't have Python installed you can find it <a href="https://www.python.org/downloads/">here</a>. The code is in the Jupyter notebook <b>epic_games_store_scraping.ipynb</b>. To run the code you need to install the following libraries:</p>
+<ul>
+<li><b>requests</b> - To make requests and download the HTML content of the pages</li>
+<li><b>BeautifulSoup</b> - To parse the HTML content and extract the data</li>
+<li><b>pandas</b> - To store the data in a structured format and save it to a CSV file</li>
+</ul>
+
+<h2>Run</h2>
+<p>To run the code you need to open the Jupyter notebook <b>epic_games_store_scraping.ipynb</b> and run all the cells. The data will be stored in the file <b>epic_games_store.csv</b>.</p>
+
+<h2>References</h2>
+<ul>
+<li><a href="https://www.crummy.com/software/BeautifulSoup/bs4/doc/">Beautiful Soup Documentation</a></li>
+<li><a href="https://www.crummy.com/software/BeautifulSoup/bs4/doc/">Beautiful Soup Documentation</a></li>
+<li><a href="https://www.crummy.com/software/BeautifulSoup/bs4/doc/">Beautiful Soup Documentation</a></li>
+</ul>
+
+<h2>License</h2>
+<p>This project is licensed under the MIT License - see the <a href="">LICENSE.md</a> file for details</p>
--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 
-if __name__ == '__main__':
-    # Substitua pelo caminho real para o seu executável do ChromeDriver
+
+def configuracaoDriver():
+    # Caminho do executável do ChromeDriver
     chrome_path = 'C:\Desenvolvimento\workspace\WebScrapEpicGames\chromedriver.exe'
 
     # Configuração do WebDriver do Chrome
     chrome_service = ChromeService(chrome_path)
-    driver = webdriver.Chrome(service=chrome_service)
+    driver_config = webdriver.Chrome(service=chrome_service)
 
+    # URL do site para fazer o Web Scraping
     URL_BASE = "https://store.epicgames.com/en-US/?lang=en-US"
 
     # Abre a URL no navegador
-    driver.get(URL_BASE)
+    driver_config.get(URL_BASE)
 
     # Encontrar os elementos que contêm os jogos gratuitos
-    freeGames = driver.find_elements("class name", "css-1ukp34s")
-    count = len(freeGames)
+    games = driver_config.find_elements("class name", "css-1ukp34s")
 
+    return driver_config, games
+
+
+def execucaoPrincipal(div_games):
     with open('games.txt', 'a') as arquivoEscrita:
-        for game in freeGames:
-            gamesText = game.text
+        for game in div_games:
+            gameInfos = game.text
 
-            startIndex = gamesText.find('FREE NOW')
-            endIndex = gamesText.find('Free Now')
-            name = gamesText[(startIndex + 9):(endIndex - 1)]
+            startIndex = gameInfos.find('FREE NOW')
+            endIndex = gameInfos.find('Free Now')
+            gameName = gameInfos[(startIndex + 9):(endIndex - 1)]
 
-            if "Unlocking" not in name:
-                print(name)
+            indexDate = gameInfos.rfind('at')
+            gameDate = gameInfos[(indexDate - 7):(indexDate + 11)]
+
+            if "Unlocking" not in gameName:
+                print("Jogos Gratuitos\n" + gameName + '\n')
+
                 with open('games.txt', 'r') as arquivoLeitura:
                     jogosLidos = arquivoLeitura.read()
-                    if name not in jogosLidos:
-                        arquivoEscrita.write(name + '\n')
+
+                    if gameName not in jogosLidos:
+                        arquivoEscrita.write(gameName + ' - ' + gameDate + '\n')
+                        print("Jogo adicionado.")
                     else:
-                        print("Jogo já adicionado")
+                        print("Jogo já adicionado.")
+    return
+
+
+if __name__ == '__main__':
+    driver, divGames = configuracaoDriver()
+
+    execucaoPrincipal(divGames)
 
     # Fechar o navegador
     driver.quit()
