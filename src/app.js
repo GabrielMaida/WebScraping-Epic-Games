3f78714765e20ffd58f566a848235a885e3934b4
Gabriel Antonio <gabriel.antonio.dlm@gmail.com>
2024-09-17 22:33:36 -0300

    Importações

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
 const axios = require('axios');
 const express = require('express');
 const cheerio = require('cheerio');
+
+async function requisicao(){
+    try{
+        // Faz a requisição para o site e obtém o HTML
+        const response = await axios.get('https://www.pcgamer.com/epic-games-store-free-games-list/');
+        const html = response.data;
+        
+        // Carrega o HTML com o Cheerio
+        const $ = cheerio.load(html);
+        
+        // Seleciona todos os elementos <ul> na página
+        const ulElements = $('ul');
+        
+        
+    } catch (error){
+
+    }
+}
+
+requisicao();
