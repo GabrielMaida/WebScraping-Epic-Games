337a5290b3d12c3285c5d16a2a29d8c32a166aba
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2024-12-16 17:42:47 -0300

    update: Função para remoção da data dos jogos e disponibilização através de um Array

--- src/app.js
+++ src/app.js
@@ -10,43 +10,76 @@ if (!process.env.WEBSITE_URL){
 }
 const URL = process.env.WEBSITE_URL;
 
-function getGames($) {
+function organizeGames($){
     const currentGame = $('#article-body>p:nth-child(13)').text();
-    // console.log(currentGame);
+    console.log(currentGame);
 
     const nextGame = $('#article-body>p:nth-child(17)').text();
-    // console.log(nextGame);
+    console.log(nextGame);
 
-    const games2024 = $('#article-body>ul:nth-child(23)').children('li').text();
-    // console.log(games2024);
+    /*
+    for (let i = 2019; i < 2025; i++) {
+        let year = i;
 
-    const games2023 = $('#article-body>ul:nth-child(25)').children('li').text();
-    // console.log(games2023);
+        switch (year) {
+            case 2019:
+                gamesRaw = $('#article-body>ul:nth-child(33)').children('li').text();
+                break;
+            case 2020:
+                gamesRaw = $('#article-body>ul:nth-child(31)').children('li').text();
+                break;
+            case 2021:
+                gamesRaw = $('#article-body>ul:nth-child(29)').children('li').text();
+                break;
+            case 2022:
+                gamesRaw = $('#article-body>ul:nth-child(27)').children('li').text();
+                break;
+            case 2023:
+                gamesRaw = $('#article-body>ul:nth-child(25)').children('li').text();
+                break;
+            case 2024:
+                gamesRaw = $('#article-body>ul:nth-child(23)').children('li').text();
+                break;
+            default:
+                console.log("Incorrect year parameter");
+                break;
+        }
+        console.log(gamesRaw);
 
-    const games2022 = $('#article-body>ul:nth-child(27)').children('li').text();
-    // console.log(games2022);
+        let listedGames = gamesRaw.split(`${year}: `);
+        console.log(listedGames);
 
-    const games2021 = $('#article-body>ul:nth-child(29)').children('li').text();
-    // console.log(games2021);
+        /*let split = listedGames[0].split(`${year + 1}: `);
+        listedGames[0] = split[1];
 
-    const games2020 = $('#article-body>ul:nth-child(31)').children('li').text();
-    // console.log(games2020);
+        split = listedGames.at(-1).split(`${year - 1}: `);
+        listedGames[listedGames.length - 2] = split[0];
+        listedGames[listedGames.length - 1] = split[1];
 
-    const games2019 = $('#article-body>ul:nth-child(33)').children('li').text();
-    // console.log(games2019);
+        let months = [/Jan /, /Feb /, /Mar /, /Apr /, /May /, /Jun /, /June /, /Jul /, /July /, /Aug /, /Sep /, /Oct /, /Nov /, /Dec /, /December /];
+
+        listedGames = listedGames.map(game => {
+            for (let i = 0; i < months.length; i++) {
+                if(game.search(months[i]) !== -1){
+                    let part = game.split(months[i]);
+                    return part[0];
+                }
+            }
+            return game;
+        });
+        console.log('Free games of', year + ':\n', listedGames);
+    }*/
 }
 
 async function getResponse(websiteURL) {
     try {
         // Make the request and save the result in 'response'
         const response = await cheerio.fromURL(websiteURL);
-        console.log('Response sucess');
 
         // Load the HTML content of the response into Cheerio
         const loadedData = cheerio.load(response.html())
-        console.log('Loading sucess')
 
-        getGames(loadedData);
+        organizeGames(loadedData);
         return 0;
         
     } catch (error) {
